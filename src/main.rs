98eb04970f51097eb85669a6770986ab9b140746
Pedro Soares <pmbs.123@gmail.com>
2026-05-15 17:31:05 +0200

    feat: add custom icon to the app

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,20 @@ mod ui;
 fn main() {
     tracing_subscriber::fmt()
         .with_env_filter(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "warn".into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "warn".into()),
         )
         .init();
 
+    let icon = eframe::icon_data::from_png_bytes(include_bytes!("../assets/blackbird-icon.png"))
+        .expect("The icon data must be valid");
+
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([1200.0, 800.0]),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1200.0, 800.0])
+            .with_icon(icon),
         ..Default::default()
     };
+
     eframe::run_native(
         "Blackbird",
         options,
