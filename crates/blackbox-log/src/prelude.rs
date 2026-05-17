5f23acc7bc9cc9c87ed85b64876607d80cadad03
Pedro Soares <pmbs.123@gmail.com>
2026-05-17 15:20:52 +0200

    build: map blackbox-log create to patch

--- crates/blackbox-log/src/prelude.rs
+++
@@ -1,8 +0,0 @@
-//! Minimal set of imports for convenience.
-//!
-//! [`File`](`crate::File`) is not included due to avoid collisions with other
-//! types.
-
-pub use crate::frame::{Frame as _, FrameDef as _};
-pub use crate::units::{Flag as _, FlagSet as _};
-pub use crate::{DataParser, Headers, ParserEvent};
