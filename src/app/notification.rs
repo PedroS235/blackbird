2f5c58272d0c267016daf7ebb80289ab404c09fc
Pedro Soares <pmbs.123@gmail.com>
2026-08-15 12:47:18 +0200

    refactor: update deprecated function call show_inside

--- src/app/notification.rs
+++ src/app/notification.rs
@@ -13,7 +13,7 @@ pub(crate) struct Notification {
 
 impl BlackbirdApp {
     pub(super) fn show_notifications(&mut self, ui: &mut egui::Ui) {
-        egui::Panel::bottom("notifications_center").show_inside(ui, |ui| {
+        egui::Panel::bottom("notifications_center").show(ui, |ui| {
             if let Some(n) = self.notifications.back() {
                 let color = match n.level {
                     Level::Error => egui::Color32::RED,
